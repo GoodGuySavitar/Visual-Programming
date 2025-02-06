482277deaf6aa8f27889c6e2c2e62d777fb9b11e
Asmit Singh Chauhan <blr.asmit@gmail.com>
2025-02-06 08:12:57 +0530

    BMI Calculator

---
+++ BMI Calculator/BMI Calculator/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(264, 103)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(158, 22)
+        Me.txtHeight.TabIndex = 0
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(264, 131)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(158, 22)
+        Me.txtWeight.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(264, 188)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(104, 31)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate BMI"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(261, 246)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 17)
+        Me.lblResult.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(162, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter Height"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(162, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Enter Weight"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ BMI Calculator/BMI Calculator/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim height As Double
+        Dim weight As Double
+
+        If Not Double.TryParse(txtHeight.Text, height) OrElse Not Double.TryParse(txtWeight.Text, weight) Then
+            MessageBox.Show("Please enter valid numerical values for Height and Weight.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        height = height / 100
+
+        If height <= 0 Or weight <= 0 Then
+            MessageBox.Show("Height and Weight must be positive numbers.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim bmi As Double = weight / (height * height)
+
+        Dim category As String = ""
+        If bmi < 18.5 Then
+            category = "Underweight"
+        ElseIf bmi >= 18.5 And bmi < 24.9 Then
+            category = "Normal weight"
+        ElseIf bmi >= 25 And bmi < 29.9 Then
+            category = "Overweight"
+        Else
+            category = "Obese"
+        End If
+
+        lblResult.Text = "BMI: " & Math.Round(bmi, 2).ToString() & " - " & category
+    End Sub
+
+
+
+End Class
