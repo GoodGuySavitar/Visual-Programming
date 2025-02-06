72011b591dc6df0a24cfc8cad84083cfd6edeeb4
Asmit Singh Chauhan <blr.asmit@gmail.com>
2025-02-06 08:26:19 +0530

    SQL Database integration
    Q no. 66

---
+++ SQL DaraGridView/SQL DaraGridView/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBookID = New System.Windows.Forms.TextBox()
+        Me.txtBookTitle = New System.Windows.Forms.TextBox()
+        Me.txtAuthor = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtBookID
+        '
+        Me.txtBookID.Location = New System.Drawing.Point(354, 97)
+        Me.txtBookID.Name = "txtBookID"
+        Me.txtBookID.Size = New System.Drawing.Size(169, 22)
+        Me.txtBookID.TabIndex = 0
+        '
+        'txtBookTitle
+        '
+        Me.txtBookTitle.Location = New System.Drawing.Point(354, 137)
+        Me.txtBookTitle.Name = "txtBookTitle"
+        Me.txtBookTitle.Size = New System.Drawing.Size(169, 22)
+        Me.txtBookTitle.TabIndex = 1
+        '
+        'txtAuthor
+        '
+        Me.txtAuthor.Location = New System.Drawing.Point(354, 179)
+        Me.txtAuthor.Name = "txtAuthor"
+        Me.txtAuthor.Size = New System.Drawing.Size(169, 22)
+        Me.txtAuthor.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(193, 236)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(274, 236)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(355, 236)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 5
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(436, 236)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisplay.TabIndex = 6
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(517, 236)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(598, 236)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBooks.Location = New System.Drawing.Point(218, 265)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.RowHeadersWidth = 51
+        Me.dgvBooks.RowTemplate.Height = 24
+        Me.dgvBooks.Size = New System.Drawing.Size(420, 195)
+        Me.dgvBooks.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(246, 101)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Book ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(246, 137)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Book Title"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(246, 179)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 17)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Author"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label4.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label4.Location = New System.Drawing.Point(362, 31)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(149, 29)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Book Details"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(868, 502)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtAuthor)
+        Me.Controls.Add(Me.txtBookTitle)
+        Me.Controls.Add(Me.txtBookID)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBookID As TextBox
+    Friend WithEvents txtBookTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents dgvBooks As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ SQL DaraGridView/SQL DaraGridView/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Dim conn As New MySqlConnection("server=localhost;user id=root;password=root;database=BookDB")
+
+    Private Sub DisplayData()
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM Books", conn)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        dgvBooks.DataSource = table
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim query As String = "INSERT INTO Books (BookTitle, Author) VALUES (@BookTitle, @Author)"
+        Dim cmd As New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@BookTitle", txtAuthor.Text)
+        cmd.Parameters.AddWithValue("@Author", txtBookTitle.Text)
+
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+        MessageBox.Show("Book Added Successfully!")
+        DisplayData()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim query As String = "UPDATE Books SET BookTitle=@BookTitle, Author=@Author WHERE BookID=@BookID"
+        Dim cmd As New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@BookID", txtBookID.Text)
+        cmd.Parameters.AddWithValue("@BookTitle", txtAuthor.Text)
+        cmd.Parameters.AddWithValue("@Author", txtBookTitle.Text)
+
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+        MessageBox.Show("Book Updated Successfully!")
+        DisplayData()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim query As String = "DELETE FROM Books WHERE BookID=@BookID"
+        Dim cmd As New MySqlCommand(query, conn)
+
+        cmd.Parameters.AddWithValue("@BookID", txtBookID.Text)
+
+        conn.Open()
+        cmd.ExecuteNonQuery()
+        conn.Close()
+
+        MessageBox.Show("Book Deleted Successfully!")
+        DisplayData()
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        DisplayData()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtBookID.Clear()
+        txtAuthor.Clear()
+        txtBookTitle.Clear()
+    End Sub
+
+End Class
